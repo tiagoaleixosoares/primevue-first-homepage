5896b3db6cadfbe6bfe33d1efbf109644609f16f
Tiago Soares <tiago.sousasoares@edp.com>
2023-04-25 15:04:09 +0100

    Added PrimeVue Stuff

--- primevue-test/src/App.vue
+++ primevue-test/src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App by Tiago to do PRimeVUe"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  <div>
+  <InputText />
+  <Botao-temp label="pi pi-check Ola"/>
+  <RichTextEditor v-model="value" editorStyle="height: 320px" />
+  
+  <DropdownTemp v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select a City" class="w-full md:w-14rem" />
+  
+  </div>
+  </template>
+  
+  <script>
+  
+  export default {
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
+  
--- primevue-test/src/main.js
+++ primevue-test/src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
 
-createApp(App).mount('#app')
+import "primevue/resources/themes/lara-light-indigo/theme.css";     
+import "primevue/resources/primevue.min.css";
+
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import Editor from 'primevue/editor';
+import Dropdown from 'primevue/dropdown';
+
+const app = createApp(App);
+app.use(PrimeVue);
+
+app.component('InputText',InputText);
+app.component('Botao-temp',Button);
+app.component('RichTextEditor',Editor);
+app.component('DropdownTemp',Dropdown);
+
+app.mount('#app');
