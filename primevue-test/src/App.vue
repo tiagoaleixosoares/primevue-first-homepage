7674098053688023b2b18218bd2244d761acb387
Tiago Soares <tiago.sousasoares@edp.com>
2023-04-25 18:59:20 +0100

    sync

--- primevue-test/src/App.vue
+++ primevue-test/src/App.vue
@@ -28,21 +28,6 @@
 
 <script>
 
-import { ref } from "vue";
-
-const selectedCountry = ref();
-const countries = ref([
-    { name: 'Australia', code: 'AU' },
-    { name: 'Brazil', code: 'BR' },
-    { name: 'China', code: 'CN' },
-    { name: 'Egypt', code: 'EG' },
-    { name: 'France', code: 'FR' },
-    { name: 'Germany', code: 'DE' },
-    { name: 'India', code: 'IN' },
-    { name: 'Japan', code: 'JP' },
-    { name: 'Spain', code: 'ES' },
-    { name: 'United States', code: 'US' }
-]);
 import axios from "axios";
 
 
